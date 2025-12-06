608d586f13bf0a246bbdde19a7a559ebb1ec6403
TokyoYoo <apocalypse21345@gmail.com>
2025-12-06 22:38:15 +0700

    Add support for new game PlaceId

--- Trst.lua
+++ Trst.lua
@@ -2,6 +2,8 @@ if game.PlaceId == 105555311806207 then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/TokyoYoo/gga2/refs/heads/main/Buildazoo.lua'))()
 elseif game.PlaceId == 129827112113663 then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/TokyoYoo/gga2/refs/heads/main/Prospecting.lua'))()
+elseif game.PlaceId == 79546208627805 then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/TokyoYoo/gga2/refs/heads/main/99night.lua'))()
 else
 	print("Noob")
 end
