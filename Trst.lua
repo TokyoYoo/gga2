9034c59e49d2ffc226c3b93c7c1275559c25628e
TokyoYoo <apocalypse21345@gmail.com>
2025-12-06 22:42:39 +0700

    Update Trst.lua

--- Trst.lua
+++ Trst.lua
@@ -1,9 +1,12 @@
-if game.PlaceId == 105555311806207 then
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/TokyoYoo/gga2/refs/heads/main/Buildazoo.lua'))()
-elseif game.PlaceId == 129827112113663 then
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/TokyoYoo/gga2/refs/heads/main/Prospecting.lua'))()
-elseif game.PlaceId == 79546208627805 then
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/TokyoYoo/gga2/refs/heads/main/99night.lua'))()
+local place = game.PlaceId
+local creator = game.CreatorId
+
+if place == 105555311806207 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/TokyoYoo/gga2/refs/heads/main/Buildazoo.lua'))()
+elseif place == 129827112113663 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/TokyoYoo/gga2/refs/heads/main/Prospecting.lua'))()
+elseif creator == 123456789 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/TokyoYoo/gga2/refs/heads/main/99night.lua'))()
 else
-	print("Noob")
+    print("Noob")
 end
